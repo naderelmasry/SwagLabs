07c04997ceeef918fce3eb5ebb0691e00ec939ac
Nader <Nader.elmasry707@gmail.com>
2024-11-13 15:34:24 +0200

    Commit message

--- src/test/java/AddProductTestCases.java
+++ src/test/java/AddProductTestCases.java
@@ -6,9 +6,7 @@ import io.qameta.allure.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
